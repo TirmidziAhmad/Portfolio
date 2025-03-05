3ab6740b57130e6e331c92689c2f00b5f51d9110
Tirmidzi Ahmad <tirmidziahmad05@gmail.com>
2025-03-06 02:14:29 +0700

    mvp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,9 @@
-import type { Metadata } from 'next';
-import localFont from 'next/font/local';
-import './globals.css';
-import Link from 'next/link';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
+import type { Metadata } from "next";
+import "./globals.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: 'Tirmidzi Ahmad 👋',
+  title: "Tirmidzi Ahmad 👋",
   description: "Miji's Portfolio",
 };
 
@@ -25,21 +13,39 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col justify-center items-center min-h-screen`}>
-        <nav className='w-full flex justify-between items-center absolute top-0 px-5 md:px-10 md:pt-4'>
-          <Link className='text-2xl' href='/'>
-            MIJI
+    <html lang="en">
+      <body className={`antialiased flex flex-col justify-center items-center min-h-screen`}>
+        <nav className="w-full flex justify-between items-center absolute top-0 px-5 md:px-10 md:pt-4 lg:px-15">
+          <Link className="text-2xl" href="/">
+            MIJI<span className="text-sm pt-auto">®</span>
           </Link>
-          <div className='flex gap-7 justify-center items-center display-none'>
-            <Link href='/about'>About</Link>
-            <Link href='/'>My Work</Link>
-            <Link href='/'>Contact</Link>
+          <div className="flex gap-7 justify-center items-center display-none">
+            <Link href="/about">About</Link>
+            <Link href="/">My Work</Link>
+            <Link href="/">Contact</Link>
           </div>
         </nav>
-        <div className='flex-grow flex flex-col justify-center items-center z-50'>{children}</div>
-        <footer>
-          <p>lets chat</p>
+        <div className="flex-grow flex flex-col justify-center items-center z-50 px-5 md:px-10 md:pt-4 lg:px-15">{children}</div>
+        <footer className="w-full flex flex-col bg-[#EDEDED] h-[70vh] text-[#1E201E] justify-center items-center gap-10">
+          <button className="text-8xl font-bold pt-1 hover:underline">
+            <a href="mailto:tirmidziahmad.work@gmail.com">LETS TALK</a>
+          </button>
+          <div>
+            <div className="flex flex-col">
+              <button>
+                <a href="mailto:tirmidziahmad.work@gmail.com">Email</a>
+              </button>
+              <button>
+                <a href="https://www.linkedin.com/in/tirmidziahmad/">Linkedin</a>
+              </button>
+              <button>
+                <a href="https://github.com/TirmidziAhmad">Github</a>
+              </button>
+              <button>
+                <a href="https://www.instagram.com/tirmidziahmd/">Instagram</a>
+              </button>
+            </div>
+          </div>
         </footer>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,110 @@
+"use client";
+import { FaReact } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiExpress, SiJavascript, SiTypescript, SiSwagger, SiPostman } from "react-icons/si";
+import Image from "next/image";
+import me from "../../public/me.png";
+import jakarta from "../../public/jakarta.jpg";
+import fullstack from "../../public/fullstack.webp";
+import coding from "../../public/coding.jpg";
+import functionality from "../../public/functionality.jpg";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { GrMysql } from "react-icons/gr";
 export default function Home() {
   return (
     <div>
-      <section id='hero' className='flex flex-col justify-center items-center h-[100vh]'>
-        <h1 className='text-6xl md:text-[180px] font-semibold --font-geist-sans'>TIRMIDZI</h1>
-        <p className='text-slate-400 text-lg md:text-xl'>FRONT END ENGINEER</p>
-        <h1 className='text-6xl md:text-[180px] font-semibold'>AHMAD</h1>
+      <section id="hero" className="flex flex-col justify-center items-center h-[100vh]">
+        <h1 className="text-6xl md:text-[180px] font-semibold ">TIRMIDZI</h1>
+        <p className="text-slate-400 text-lg md:text-xl">FULLSTACK DEV | FRONT END DEV</p>
+        <h1 className="text-6xl md:text-[180px] font-semibold">AHMAD</h1>
       </section>
-      <section className='flex flex-col justify-center items-center h-[50vh]'>
-        <h2 className='text-lg md:text-6xl'>Tirmidzi Ahmad is Indonesian</h2>
-        <h2 className='text-lg md:text-6xl'>Front End Engineer,</h2>
-        <h2 className='text-lg md:text-6xl text-slate-500'>Who turns creativity</h2>
-        <h2 className='text-lg md:text-6xl text-slate-500'>into functionality</h2>
+
+      <section id="about" className="flex flex-col justify-center items-center h-[50vh] gap-4 font-normal">
+        <h2 className="text-md md:text-6xl">
+          <span className="flex gap-2">
+            Tirmidzi Ahmad <Image src={me} alt="me" width={60} height={60} /> is Indonesian <Image src={jakarta} alt="me" width={90} height={90} className="rounded-md" objectFit="cover" />
+          </span>
+        </h2>
+        <h2 className="text-md md:text-6xl flex gap-2">
+          Experienced web developer <Image src={fullstack} alt="me" width={90} height={90} className="rounded-md" objectFit="cover" />,
+        </h2>
+        <h2 className="text-lg md:text-6xl text-slate-500 flex gap-2">
+          Who turns <Image src={coding} alt="me" width={90} height={90} className="rounded-md" objectFit="cover" /> ideas
+        </h2>
+        <h2 className="text-lg md:text-6xl text-slate-500 flex gap-2">
+          into <Image src={functionality} alt="me" width={90} height={90} className="rounded-md" objectFit="cover" /> functionality.
+        </h2>
       </section>
-      <section id='tech-stack' className='h-[90vh] flex items-center justify-center'>
-        <div className='grid grid-cols-5 grid-rows-5 gap-4 justify-center items-center border-white text-center '>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div className='col-start-5 row-start-2'>6</div>
-          <div className='col-start-4 row-start-2'>7</div>
-          <div className='col-start-5 row-start-3'>8</div>
-          <div className='col-start-5 row-start-4'>9</div>
-          <div className='col-start-4 row-start-4'>10</div>
-          <div className='col-start-4 row-start-3'>11</div>
-          <div className='col-start-1 row-start-2'>12</div>
-          <div className='col-start-1 row-start-3'>13</div>
-          <div className='col-start-1 row-start-4'>14</div>
-          <div className='col-start-2 row-start-2'>15</div>
-          <div className='col-start-2 row-start-3'>16</div>
-          <div className='col-start-2 row-start-4'>17</div>
-          <div className='col-start-3 row-start-4'>18</div>
-          <div className='row-span-2 col-start-3 row-start-2 uppercase'>
-            Tech Stack <br />
-            that i use <br />
-            for development
+      <section id="tech-stack" className="h-[90vh] flex flex-col items-center justify-center gap-2">
+        <h2 className="text-lg md:text-6xl">Tech that i have experience with</h2>
+        <p className="flex flex-col md:text-6xl">
+          <div className="flex  items-center justify-center  gap-2  text-slate-500">
+            <span className="hover:text-blue-500 flex">
+              React <FaReact />
+            </span>
+            ,
+            <span className="hover:text-black flex gap-3">
+              Next <RiNextjsFill />
+            </span>
+            ,
+            <span className="hover:text-white flex gap-3">
+              Express <SiExpress />
+            </span>
+          </div>
+          <div className="flex items-center justify-center gap-2 text-slate-500">
+            <span className="flex hover:text-blue-400 gap-3">
+              postgresql <BiLogoPostgresql />
+            </span>
+            ,
+            <span className="flex hover:text-blue-400 gap-3">
+              mysql <GrMysql />
+            </span>
+            ,
+            <span className="flex hover:text-yellow-300 gap-3">
+              javascipt <SiJavascript />,
+            </span>
+          </div>
+          <div className="flex items-center justify-center gap-2 text-slate-500">
+            <span className="flex hover:text-blue-400 gap-3">
+              typescript <SiTypescript />
+            </span>
+            ,
+            <span className="flex hover:text-green-400 gap-3">
+              swagger <SiSwagger />
+            </span>
+            ,
+            <span className="flex hover:text-orange-400 gap-3">
+              postman <SiPostman />
+            </span>
+          </div>
+        </p>
+      </section>
+      <section id="my-work" className="h-[90vh] flex flex-col items-center justify-center">
+        <h2 className="text-lg md:text-6xl mb-3">Personal Project</h2>
+        <div className="flex gap-5">
+          <div className="bg-[#1e201e] rounded-lg border border-[#85858526] p-5 ">
+            <h4 className="text-xl capitalize">Rice disease detection</h4>
+            <p>app to detect rice disease expert system, built with next js and postgresql as database</p>
+            <button className="rounded-lg bg-transparent border border-[#85858526] p-2">
+              <a href="">visit</a>
+            </button>
+          </div>
+          <div className="bg-[#1e201e] rounded-lg border border-[#85858526] p-5">
+            <h4 className="text-xl capitalize">chill-Netflix clone</h4>
+            <p>fullstack netflix clone app built with react and express using zustand for front end state management</p>
+            <button className="rounded-lg bg-transparent border border-[#85858526] p-2">
+              <a href="https://chill-netflix-clone.vercel.app/">visit</a>
+            </button>
+          </div>
+          <div className="bg-[#1e201e] rounded-lg border border-[#85858526] p-5 ">
+            <h4 className="text-xl ">Si Paling persegi</h4>
+            <p>An app to determine area and perimeter of a square bulit bith HTML,CSS, and javascirpt</p>
+            <button className="rounded-lg bg-transparent border border-[#85858526] p-2">
+              <a href="https://tirmidziahmad.github.io/SiPalingPersegi/">visit</a>
+            </button>
           </div>
         </div>
       </section>
-
-      <section id='my-work'>Works</section>
     </div>
   );
 }
